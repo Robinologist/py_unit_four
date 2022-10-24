00863d6d44fe319482a997070eed9eadaeda36ec
Robinologist <112644607+Robinologist@users.noreply.github.com>
2022-10-24 15:00:30 -0400

    Created a function to find out if a user-inputted number is even or odd

--- even_odd.py
+++ even_odd.py
@@ -1,19 +1,13 @@
 
 def even_or_odd(number):
-    """
-    Create a function that will tell if a number is even or odd. Use two if statements to do this.
-    :param number: could be any positive or negative integer
-    :return: either "x is an even number" or "x is an odd number"
-    """
-    pass
-
+    if int(number) % 2 == 0:
+        print("the number", number, "is even")
+    if int(number) % 2 == 1:
+        print("the number", number, "is odd")
 
 def main():
-    # First, make sure to delete the word "pass" then get input from the user.
-    # They should type in a number, make sure to convert it to an int
-    # Next, call the even_or_odd function, and make sure to pass the user's number as a parameter.
-    pass
-
+    number = input("")
+    even_or_odd(number)
 
 if __name__ == '__main__':
     main()
